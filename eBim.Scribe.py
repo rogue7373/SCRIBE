86493e496d6b07f395de1de0182f6a3a0bc233d6
Christian Paustell <55620059+cp8520@users.noreply.github.com>
2024-03-05 18:04:39 -0700

    Update eBim.Scribe.py
    Added features and GUI updates

--- eBim.Scribe.py
+++ eBim.Scribe.py
@@ -1,3 +1,4 @@
+import sys
 import flet as ft
 from zoneinfo import ZoneInfo
 from datetime import datetime
@@ -12,6 +13,7 @@ class program():
         page.window_opacity = .95
         page.auto_scroll = True
 
+        
         def insert_timestamp(event):
             pacifictime = datetime.now(ZoneInfo('America/Los_Angeles')).replace(tzinfo=None).isoformat(sep=" ",timespec="seconds")
             event.control.value += f"\n{pacifictime} >>> "
@@ -24,8 +26,7 @@ class program():
                 )
             return input_field
 
-        tabs_list = [ft.Tab(text="eBim", content=ft.Column([create_input_field()]))]
-        
+        tabs_list = []
 
         def add_tab(event):
             new_tab = ft.Tab(text=f"eBim {len(tabs_list)+1}", content=ft.Column([create_input_field()]))
@@ -33,7 +34,35 @@ class program():
             tabs_control.tabs = tabs_list  
             tabs_control.selected_index = len(tabs_list) - 1 
             page.update()
+        
+        def close_tab(event):
+              
+            tabs_control.tabs = tabs_list  
+            tabs_control.selected_index = len(tabs_list)
+            tabs_list.pop()
+            page.update()
 
+        def close_app(event):
+            print("exit")
+            sys.exit()
+
+        def check_item_clicked(e):
+                    e.control.checked = not e.control.checked
+                    page.update()
+
+        page.appbar = ft.AppBar(
+            leading=ft.Icon(ft.icons.NOTE_ALT),
+            leading_width=40,
+            title=ft.Text("eBim Scribe"),
+            center_title=True,
+            bgcolor=ft.colors.SURFACE_VARIANT,
+            actions=[
+                ft.IconButton(ft.icons.PLAYLIST_ADD, on_click=add_tab),
+                ft.IconButton(ft.icons.PLAYLIST_REMOVE, on_click=close_tab),
+                ft.IconButton(ft.icons.CLOSE, on_click=close_app),
+                
+            ],
+        )
         tabs_control = ft.Tabs(
             selected_index=0,
             animation_duration=300,
@@ -41,8 +70,10 @@ class program():
             expand=0,            
         
         )
-        add_tab_button = ft.IconButton(icon="ADD", on_click=add_tab)
-        page.add(add_tab_button)
+        add_tab_button = ft.IconButton(icon="PLAYLIST_ADD", on_click=add_tab)
+        add_close_app_button = ft.IconButton(icon="CLOSE", on_click=close_app,)
+        add_close_tab_button = ft.IconButton(icon="PLAYLIST_REMOVE", on_click=close_tab,)
+
         page.add(tabs_control)
 
 
